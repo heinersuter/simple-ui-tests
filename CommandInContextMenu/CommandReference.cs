e00edd07220e7ebf7e0d74b09c4baed4df3c8c59
Heiner Suter <h.suter@sma-partner.ch>
2014-04-08 13:16:19 +0200

    Command in ContextMenu.

---
+++ CommandInContextMenu/CommandReference.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CommandInContextMenu {
+    public class CommandReference : FrameworkElement, ICommand {
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+            "Command",
+            typeof(ICommand),
+            typeof(CommandReference),
+            new PropertyMetadata(OnCommandChanged));
+
+        public ICommand Command {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty = DependencyProperty.Register(
+            "CommandParameter",
+            typeof(object),
+            typeof(CommandReference),
+            new PropertyMetadata(OnCommandParameterChanged));
+
+        public object CommandParameter {
+            get { return GetValue(CommandParameterProperty); }
+            set {SetValue(CommandParameterProperty, value);}
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) {
+            return Command != null && Command.CanExecute(parameter);
+        }
+
+        public void Execute(object parameter) {
+            Command.Execute(parameter);
+        }
+
+        private static void OnCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            var commandReference = (CommandReference)d;
+            var oldCommand = e.OldValue as ICommand;
+            var newCommand = e.NewValue as ICommand;
+
+            if (oldCommand != null) {
+                //oldCommand.CanExecuteChanged -= commandReference.CanExecuteChanged;
+                oldCommand.CanExecuteChanged -= commandReference.CanExecuteChanged;
+            }
+            if (newCommand != null) {
+                //newCommand.CanExecuteChanged += commandReference.CanExecuteChanged;
+                newCommand.CanExecuteChanged += commandReference.OnCanExecuteChanged;
+            }
+        }
+
+        private static void OnCommandParameterChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            var commandReference = (CommandReference)d;
+            commandReference.OnCanExecuteChanged(d, EventArgs.Empty);
+        }
+
+        private void OnCanExecuteChanged(object sender, EventArgs eventArgs) {
+            if (CanExecuteChanged != null) {
+                CanExecuteChanged.Invoke(this, eventArgs);
+            }
+        }
+    }
+}
+
---
+++ CommandInContextMenu/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+
+namespace CommandInContextMenu {
+    public partial class MainWindow {
+        private Item _selectedItem;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            Items = new ArrayList{
+                new Item { A = "Hello", B ="World" },
+                new Item { A = "X1", B ="X2" },
+            };
+
+            WriteCommand = new DelegateCommand(Write, CanWrite);
+
+            DataContext = this;
+        }
+
+        public IList Items { get; private set; }
+
+        public Item SelectedItem {
+            get {
+                return _selectedItem;
+            }
+            set {
+                _selectedItem = value;
+                WriteCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public DelegateCommand WriteCommand { get; private set; }
+
+        private bool CanWrite() {
+            return SelectedItem != null && SelectedItem.A.Contains("1");
+        }
+
+        private void Write() {
+            Console.Write(SelectedItem.A);
+            Console.Write(@" ");
+            Console.WriteLine(SelectedItem.B);
+        }
+
+        public class Item {
+            public string A { get; set; }
+            public string B { get; set; }
+        }
+    }
+}
