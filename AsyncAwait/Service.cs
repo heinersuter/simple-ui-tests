4886f19346ae41037524f31660bc7be5a6040007
Heiner <heiner.suter@gmx.ch>
2015-04-23 10:21:28 +0200

    Async Await example.

---
+++ AsyncAwait/Service.cs
@@ -0,0 +1,20 @@
+﻿namespace AsyncAwait
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class Service
+    {
+        public async Task<string> LoadTextAsync()
+        {
+            return await Task.Run((Func<string>)LoadText);
+        }
+
+        private string LoadText()
+        {
+            Thread.Sleep(2000);
+            return "Hello";
+        }
+    }
+}
