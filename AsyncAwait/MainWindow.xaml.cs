3d98441d19471e4bd8f74d04a18e20a50ba4b227
Heiner <heiner.suter@gmx.ch>
2015-04-27 10:29:55 +0200

    Async Await.

--- AsyncAwait/MainWindow.xaml.cs
+++ AsyncAwait/MainWindow.xaml.cs
@@ -1,49 +1,10 @@
 ﻿namespace AsyncAwait
 {
-    using System.ComponentModel;
-    using System.Runtime.CompilerServices;
-    using System.Windows;
-
-    public partial class MainWindow : INotifyPropertyChanged
+    public partial class MainWindow
     {
-        private readonly Service _service;
-        private bool _isBusy;
-
         public MainWindow()
         {
             InitializeComponent();
-            _service = new Service();
-        }
-
-        private async void Clicked(object sender, RoutedEventArgs args)
-        {
-            IsBusy = true;
-            TextBox.Text += await _service.LoadTextAsync();
-            TextBox.Text += await _service.LoadTextAsync();
-            TextBox.Text += await _service.LoadTextAsync();
-            TextBox.Text += await _service.LoadTextAsync();
-            IsBusy = false;
-        }
-
-        public bool IsBusy
-        {
-            get { return _isBusy; }
-            set
-            {
-                if (_isBusy != value)
-                {
-                    _isBusy = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            var handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ AsyncAwait/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿namespace AsyncAwait
+{
+    using Alsolos.Commons.Controls.Progress;
+    using Alsolos.Commons.Mvvm;
+
+    public class MainWindowViewModel : BusyViewModel
+    {
+        private readonly Service _service;
+
+        public MainWindowViewModel()
+        {
+            _service = new Service();
+        }
+
+        public DelegateCommand LoadCommand
+        {
+            get { return BackingFields.GetCommand(Load, CanLoad); }
+        }
+
+        public string Text
+        {
+            get { return BackingFields.GetValue<string>(); }
+            set { BackingFields.SetValue(value); }
+        }
+
+        private bool CanLoad()
+        {
+            return true;
+        }
+
+        private async void Load()
+        {
+            using (BusyHelper.Enter("Loading first text..."))
+            {
+                Text += await _service.LoadTextAsync();
+
+                using (BusyHelper.Enter("Loading second text..."))
+                {
+                    Text += await _service.LoadTextAsync();
+
+                    using (BusyHelper.Enter("Loading third text..."))
+                    {
+                        Text += await _service.LoadTextAsync();
+                    }
+                }
+                using (BusyHelper.Enter("Loading fourth text..."))
+                {
+                    Text += await _service.LoadTextAsync();
+                }
+            }
+        }
+    }
+}
